07dc81066da31b4852dd97315fbca70808c37ce7
Abdelmonam K. <monaimabdel119@gmail.com>
2026-02-16 12:29:32 +0100

    fix: add dynamic rendering to all admin pages

--- app/admin/projects/edit/[id]/page.tsx
+++ app/admin/projects/edit/[id]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { notFound } from "next/navigation";
 import ProjectForm from "@/components/ProjectForm";
 import { getProjectById } from "@/lib/projects";
